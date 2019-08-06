a7c1d516ac86e40d1bceb34626a3b6ea2bf94391
nidheeshh <44805816+nidheeshh@users.noreply.github.com>
2019-08-06 14:54:11 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,5 +1,5 @@
 ###test ## make a change
 ##again making changes
-mkdir /root/test && cd test
+mkdir /root/test && cd /root/test/
 touch test1.txt
 echo "Hello, this file is created to test Jenkins with Github" >> test1.txt
